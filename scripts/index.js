6657f69c04085816c83227d85d8d4b290ff402fd
Md. Abdullah Al Ahad <abdullah.ruet22@gmail.com>
2025-09-05 04:58:16 +0600

    add lessons words in the container

--- scripts/index.js
+++ scripts/index.js
@@ -3,14 +3,60 @@ const loadLessons = () => {
     .then((res) => res.json())
     .then((json) => displayLessons(json.data));
 };
-
+const loadLevelWord = (id) => {
+  const url = `https://openapi.programming-hero.com/api/level/${id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayLevelWord(data.data));
+};
+const displayLevelWord = (words) => {
+  const wordContainer = document.getElementById("word-container");
+  wordContainer.innerHTML = "";
+  if (words.length == 0) {
+    wordContainer.innerHTML = `
+    <div class="col-span-full text-center">
+        <img src="./assets/alert-error.png" alt="" class="mx-auto"/>
+        <p
+          class="text-xl font-medium text-gray-400 rounded-xl py-10 space-y-6 hind-siliguri"
+        >
+          এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।
+        </p>
+        <h2 class="font-bold text-4xl hind-siliguri">
+          নেক্সট Lesson এ যান
+        </h2>
+      </div>
+      `;
+    return;
+  }
+  words.forEach((word) => {
+    const card = document.createElement("div");
+    card.innerHTML = `
+    <div
+        class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4"
+      >
+        <h2 class="font-bold">${word.word}</h2>
+        <p class="font-semibold">Meaning/Pronunciaton</p>
+        <div class="font-medium text-2xl hind-siliguri pb-4">"${word.meaning} / ${word.pronunciation}"</div>
+        <div class="flex justify-between items-center">
+          <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF70]">
+            <i class="fa-solid fa-circle-info"></i>
+          </button>
+          <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF70]">
+            <i class="fa-solid fa-volume-high"></i>
+          </button>
+        </div>
+      </div>
+    `;
+    wordContainer.append(card);
+  });
+};
 const displayLessons = (lessons) => {
   const levelContainer = document.getElementById("level-container");
   levelContainer.innerHTML = "";
   for (let lesson of lessons) {
     const btnDiv = document.createElement("div");
     btnDiv.innerHTML = `
-    <button class="btn btn-outline btn-primary">
+    <button onclick="loadLevelWord(${lesson["level_no"]})" class="btn btn-outline btn-primary">
     <i class="fa-solid fa-book-open"></i>Lesson -${lesson["level_no"]}
     </button>
     `;
